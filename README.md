91603a80dcc082f4fc47479ff2ecc0a97dd45271
Fredric Endrerud <fredricendrerud@gmail.com>
2013-10-28 12:22:11 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,79 @@
+CanaryJS
+=====
+
+Basic logging tools.
+
+Log Levels
+----------
+
+- **DEBUG** (100)
+
+- **INFO** (200)
+
+- **NOTICE** (250)
+
+- **WARNING** (300)
+
+- **ERROR** (400)
+
+- **CRITICAL** (500)
+
+
+Usage
+-----
+
+```js
+window.canary = new CanaryJS();
+
+canary.debug('Your debug message.');
+canary.notice('Your debug message.');
+
+// loglevel( string, obj ) Pass in context.
+canary.debug('The message', {context: this, value: '...'});
+```
+
+
+### Functions for accessing the logs
+
+```js
+// getEntry( int ) Fetch entry based on id
+canary.debug('Message #1');
+canary.info('Message #2');
+canary.warning('Message #3');
+canary.getEntry(2); // Will return Object {id: 2, type: "warning", msg: "Message #3"}
+
+// dump(optional: string) dump all entries to the console in a ordered list.
+canary.dump();
+
+// flush(optional: string) dump all entries to the console and reset the log.
+canary.flush(); // similiar to calling dump() and then reset()
+
+// reset() removes all entries from the log
+canary.reset();
+```
+
+### Access to model properties
+
+```js
+canary.get('numberOfEntries'); // Returns number of entries made.
+canary.get('name'); // Returns name of collection.
+canary.get('console'); // Returns true/false based on current setting.
+canary.get('level'); // Returns current logging level (int).
+```
+
+### Default values
+
+- **name** ('CanaryJS'), Will be attached to log entries when dumping/flushing.
+
+- **level** (0), Raise this to exclude logging entries with a lower level. (See the log levels at the top of this readme.)
+
+- **console** (false), If true, CanaryJS will write a copy of the entry to the console immediately when a new entry is added.
+
+
+You can pass on default values when you create/initiale a log collection, or change them with the set() method later.
+
+```js
+window.datatablelog = new CanaryJS({name: 'Data table', level: 200, console: true});
+
+datatablelog.set('level', 400);
+```
---
+++ canary.js
@@ -0,0 +1,171 @@
+/*!
+ * Canary.js debug functions for your backbone.js powered applications
+ * Based on RFC 5424 and Seldaek/monolog
+ *
+ */
+
+var CanaryJS = Backbone.Model.extend({
+    defaults: {
+        name: 'CanaryJS',
+        level: 0,
+        console: false,
+        numberOfEntries: 0
+    },
+    
+    initialize: function () {
+        this.set('debugCollection', new CanaryDebugCollection());
+    },
+    
+    addEntry: function(msg, obj, type, level) {
+        if (this.get('level') <= level ) {
+            if (obj == 'undefined') {
+                this.get('debugCollection').add([{id: this.get('numberOfEntries'), type: type, msg: msg}]);
+                obj = '';
+            } else {
+                console.log('stuff');
+                this.get('debugCollection').add([{id: this.get('numberOfEntries'), type: type, msg: msg, obj: obj}]);
+            }
+            if (this.get('console') == true) {
+                this.get('debugCollection').writeToConsole({id: this.get('numberOfEntries'), stamp: this.get('name'), type: type, msg: msg, obj: obj}, false);
+            }
+            this.set('numberOfEntries', this.get('numberOfEntries') + 1);
+        }
+    },
+
+    debug: function(msg, obj) {
+        if (typeof obj === 'undefined') {
+            this.addEntry(msg, 'undefined', 'debug', 100);
+        } else {
+            this.addEntry(msg, obj, 'debug', 100);
+        }
+    },
+    info: function(msg, obj) {
+        if (typeof obj === 'undefined') {
+            this.addEntry(msg, 'undefined', 'info', 200);
+        } else {
+            this.addEntry(msg, obj, 'info', 200);
+        }
+    },
+    notice: function(msg, obj) {
+        if (typeof obj === 'undefined') {
+            this.addEntry(msg, 'undefined', 'notice', 250);
+        } else {
+            this.addEntry(msg, obj, 'notice', 250);
+        }
+    },
+    warning: function(msg, obj) {
+        if (typeof obj === 'undefined') {
+            this.addEntry(msg, 'undefined', 'warning', 300);
+        } else {
+            this.addEntry(msg, obj, 'warning', 300);
+        }
+    },
+    error: function(msg, obj) {
+        if (typeof obj === 'undefined') {
+            this.addEntry(msg, 'undefined', 'error', 400);
+        } else {
+            this.addEntry(msg, obj, 'error', 400);
+        }
+    },
+    critical: function(msg, obj) {
+        if (typeof obj === 'undefined') {
+            this.addEntry(msg, 'undefined', 'critical', 500);
+        } else {
+            this.addEntry(msg, obj, 'critical', 500);
+        }
+    },
+    
+    getEntry: function(id) {
+        var entry = this.get('debugCollection').get(id);
+        return {id: entry.get('id'), type: entry.get('type'), msg: entry.get('msg'), obj: entry.get('obj')};
+    },
+
+    dump: function( msg ) {
+        if (typeof msg === 'undefined') {
+            console.group(this.get('name'));
+        } else {
+            console.group(msg);
+        }
+        
+        this.get('debugCollection').walk( false );
+        console.groupEnd();
+        return '/dump';
+    },
+
+    flush: function( msg ) {
+        if (typeof msg === 'undefined') {
+            console.group(this.get('name'));
+        } else {
+            console.group(msg);
+        }
+        this.get('debugCollection').walk( true );
+        console.groupEnd();
+        this.set('numberOfEntries', 0);
+        return '/flush';
+    },
+
+    reset: function() {
+        this.set('numberOfEntries', 0);
+        this.get('debugCollection').reset();
+        return true;
+    }
+});
+
+var CanaryDebug = Backbone.Model.extend({
+    defaults: {
+        id: 0,
+        type: 'unknown',
+        msg: '',
+        obj: ''
+    }
+});
+
+var CanaryDebugCollection = Backbone.Collection.extend({
+    
+    model: CanaryDebug,
+
+    walk: function( flush ) {
+        var delEntry = [];
+        var scope = this;
+        this.map(function(CanaryDebug) {
+            var id = CanaryDebug.get('id');
+            var type = CanaryDebug.get('type');
+            var msg = CanaryDebug.get('msg');
+            var obj = CanaryDebug.get('obj');
+            scope.writeToConsole({id: id, type: type, msg: msg, obj: obj}, true);
+            delEntry.push(id);
+        });
+
+        if ( flush == true) {
+            for (var i = 0; i < delEntry.length; i++) {
+                this.remove(delEntry[i]);
+            }
+        }
+    },
+
+    writeToConsole: function(params, raw) {
+
+        if (raw == true) {
+            var meta = '';
+        } else {
+            var meta = params.stamp +'['+params.id+'] ';
+        }
+
+        switch (params.type) {
+            case 'debug':
+                console.debug(meta + params.msg, params.obj);
+            break;
+            case 'info':
+            case 'notice':
+                console.info(meta + params.msg, params.obj);
+            break;
+            case 'warning':
+                console.warn(meta + params.msg, params.obj);
+            break;
+            case 'error':
+            case 'critical':
+                console.error(meta + params.msg, params.obj);
+            break;
+        }
+    }
+});
